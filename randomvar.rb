bc8ec145d0ca98a920a976369b08519d0632011a
brittany <brittanyntackett88@gmail.com>
2017-06-28 00:20:44 -0400

    random numbers and months

--- randomvar.rb
+++ randomvar.rb
@@ -1,13 +1,41 @@
-def randomizer
-	var= rand(9) #setting a variable to random num 1-38
+# def party_randomizer(num) #function name and argument
+# 	var= rand(num)  #passing an argument instead of hardcoding the value
 	
-	if var == 3 
-		puts "shots"
-	elsif var == 9
-			puts "burn one"
-		else puts var
+# 	if var == 3 #if variable is equal to 3
+# 		puts "shots"
+# 	elsif var == 9
+# 			puts "burn one"
+# 		else puts var #if not equal puts a number
 
-end
 
-	end
-	randomizer
+# end
+
+# 	end
+# 	party_randomizer(88) #passing an argument into function
+
+
+def life_years(num)
+	var= rand(num)
+	if var==88
+		puts "birthday"
+		elsif var==4
+			puts "april"
+			elsif var==10
+				puts "october"
+				elsif var==7
+					puts "july"
+				elsif var==12
+					puts "december"
+				elsif var==5
+					puts "may"
+				elsif var==1
+					puts "january"
+				elsif var==2
+					puts "february"
+				elsif var==8
+					puts "august"
+
+				else puts var
+				end
+			end
+life_years(88)
