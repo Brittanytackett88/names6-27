e2572d07209c6aa5a1557ea2b12a6587d1849fd1
brittany <brittanyntackett88@gmail.com>
2017-06-27 23:35:27 -0400

    changed random number to a word

---
+++ randomvar
@@ -0,0 +1,5 @@
+def randomizer
+	var= rand(9)
+	puts var 
+end
+	randomizer
---
+++ randomvar.rb
@@ -0,0 +1,13 @@
+def randomizer
+	var= rand(9) #setting a variable to random num 1-38
+	
+	if var == 3 
+		puts "shots"
+	elsif var == 9
+			puts "burn one"
+		else puts var
+
+end
+
+	end
+	randomizer
